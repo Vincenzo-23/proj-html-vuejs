80bebbb4295e39b38590136e1493d08884037fa5
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-04-22 15:35:32 +0200

    ADD: added the HeaderApp component, taking voices of the navbar from the App with props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-   
+   <HeaderApp :voices="menus" />
   </div>
 </template>
 
 <script>
+import HeaderApp from "./components/HeaderApp.vue"
+
   export default {
-    
+    components:{
+      HeaderApp,
+    },
+    data(){
+      return{
+        menus: [ "Home", "Who We Are", "What We Do", "Where We Work", "Careers", "APPLY", "News"]
+      }
+    }
   }
 </script>
 
 <style lang="scss">
+
+@use "./style/general.scss"
 
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container_large">
+        <div class="row">
+            <div class="col-auto logo">
+                <img src="/images/logo_seo_w_2x.png" alt="">
+            </div>
+            <ul class="col-auto d-flex flex-row navbar">
+                <li v-for="(menu, i) in voices" :key="i">
+                    <a href="#">{{menu}}</a>
+                </li>
+            </ul>
+            <div class="col-auto">
+                <button class="button">GET IN TOUCH NOW</button>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props:["voices"]
+    }
+</script>
+
+<style lang="scss" scoped>
+.container_large{
+    padding: 30px 0;
+}
+.navbar{
+    gap: 35px;
+    padding-right: 25px;
+    
+}
+.logo{
+margin-right: auto;
+}
+li{
+    margin: 0;
+    &:first-child{
+        color: rgb(255, 167, 55);
+    }
+    &:nth-child(6){
+        color: rgb(255, 167, 55);
+        border: 1px solid rgb(255, 167, 55);
+        padding: 3px 8px;
+        border-radius: 5px;
+        font-size: 12px;
+        margin-left: -30px;
+    }
+}
+
+img{
+    height: 40px;
+    width: 160px;
+}
+button{
+    background-color: rgb(255, 167, 55);
+    border-radius: 999px;
+    padding: 10px 15px;
+    color: white;
+    font-weight: 700;
+}
+</style>
